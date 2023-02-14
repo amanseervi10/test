3a8221b537377098fa21d4fa1ba41546c834bf10
Aman <amanseervi10@gmail.com>
2023-02-14 08:35:44 +0530

    Made a new cpp file

---
+++ the.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
+    return 0;
+}
